d9ec1969de38cca313bd58b9815109f4ff586012
FOZIA QURAISHI <103851865+bitt6@users.noreply.github.com>
2024-02-02 21:57:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Ziplinker.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', '55a9b63bafac2a44eb704f938b7ad572057b6fa6')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
